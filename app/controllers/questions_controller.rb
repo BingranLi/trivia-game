5e90a580473e8566304b5bfd9fd3f8f850ba76cb
ryan <bingran.ryan.li@gmail.com>
2017-05-29 00:09:51 +0000

    add create and show question

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,3 +1,25 @@
-class QuestionsController
+class QuestionsController < ApplicationController
   
+  def new
+    @question = Question.new
+  end
+  
+  def create
+    @question = Question.new(question_params)
+    if @question.save
+      flash[:notice] = "Question was successfully created"
+      redirect_to question_path(@question)
+    else
+      render 'new'
+    end
+  end
+  
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  private
+    def question_params
+      params.require(:question).permit(:problem, :answer)
+    end
 end
